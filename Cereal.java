4dcea660d03d822af8fa8b0df248d1a0a4dd84ca
Lauringschw <42889446+Lauringschw@users.noreply.github.com>
2020-05-17 11:53:21 +0200

    Update Cereal.java

--- Cereal.java
+++ Cereal.java
@@ -164,17 +164,4 @@ public class Cereal {
 	public String toString() {
 		return "Cereal " + name + " Type:" + type + " Calories:" + calories + " Cups:" + cups + " Rating:" + rating;
 	}
-
-	// main tester
-	public static void main(String[] args) {
-		// cereal constructor wih all parameters
-		Cereal c1 = new Cereal("Frosted Flakes", "C", 110, 1, 0, 200, 1, 14, 11, 25, 25, 1, 1, .75, 31.43597);
-		System.out.println(c1.toString());
-		// cereal constructor with some parameters
-		Cereal c2 = new Cereal("Cocoa Puffs", "C", 110, 1, 1, 180, 0, 12, 13, 55, 25, 2, 1, 1, 22.73645);
-		System.out.println(c2.toString());
-		// cereal constructor without parameters
-		Cereal c3 = new Cereal();
-		System.out.println(c3.getName() + " " + c3.getRating());
-	}
 }
