c371efbff5808bd79108c4121ead0f40e4f5acfe
Lauringschw <42889446+Lauringschw@users.noreply.github.com>
2020-05-17 11:53:59 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
-
 public class Main {
 
+	//main tester
 	public static void main(String[] args) {
-		Cereal c1 = new Cereal("Cocoa Puffs", "C", 110, 1, 1, 180, 0, 12, 13, 55, 25, 2, 1, 1, 22.73645);
+		//cereal constructor wih all parameters
+		Cereal c1 = new Cereal("Frosted Flakes", "C", 110, 1, 0, 200, 1, 14, 11, 25, 25, 1, 1, .75, 31.43597);
 		System.out.println(c1.toString());
-		Cereal c2 = new Cereal("Frosted Flakes", "C", 110, 1, 0, 200, 1, 14, 11, 25, 25, 1, 1, .75, 31.43597);
+		//cereal constructor with some parameters
+		Cereal c2 = new Cereal("Cocoa Puffs", "C", 110, 1, 1, 180, 0, 12, 13, 55, 25, 2, 1, 1, 22.73645);
 		System.out.println(c2.toString());
+		//cereal constructor without parameters
 		Cereal c3 = new Cereal();
 		System.out.println(c3.getName() + " " + c3.getRating());
 	}
