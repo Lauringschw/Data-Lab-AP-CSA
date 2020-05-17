a054ce289a70303c95c70db02c8384ed7e1a7211
Lauringschw <42889446+Lauringschw@users.noreply.github.com>
2020-05-17 12:04:05 +0200

    Update Welcome03_List.java

--- Welcome03_List.java
+++ Welcome03_List.java
@@ -1,30 +1,31 @@
-import core.data.*;
-import java.util.ArrayList;
-import java.util.Scanner;
+import core.data.*; //importing data
+import java.util.ArrayList; //importing arraylist
+import java.util.Scanner; //importing scanner
 
 public class Welcome03_List {
+	// main tester
 	public static void main(String[] args) {
 		DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/index.xml").load();
 		ArrayList<WeatherStation> allstns = ds.fetchList("WeatherStation", "station/station_name", "station/station_id",
 				"station/state", "station/latitude", "station/longitude");
 		System.out.println("Total stations: " + allstns.size());
 
-		Scanner sc = new Scanner(System.in);
+		Scanner input = new Scanner(System.in); // ask user
 		System.out.println("Enter a state abbreviation: ");
-		String state = sc.next();
+		String state = input.next();
 		System.out.println("Stations in " + state);
 		int count = 0;
-		for (WeatherStation ws : allstns) {
-			if (ws.isLocatedInState(state)) {
+		for (WeatherStation weather : allstns) {
+			if (weather.isLocatedInState(state)) {
 				count++;
-				System.out.println("  " + ws.getId() + ": " + ws.getName());
+				System.out.println("  " + weather.getId() + ": " + weather.getName());
 			}
 		}
 		System.out.println("There are " + count + " stations in " + state + ".");
 		WeatherStation southern = allstns.get(0);
-		for (WeatherStation ws : allstns) {
-			if (ws.getLat() < southern.getLat()) {
-				southern = ws;
+		for (WeatherStation weather : allstns) {
+			if (weather.getLat() < southern.getLat()) {
+				southern = weather;
 			}
 		}
 		System.out.println(southern.getName() + " Latitude: " + southern.getLat());
