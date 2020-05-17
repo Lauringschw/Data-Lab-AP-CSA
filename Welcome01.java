f87febbe515c6f59cae4c97f50ec317e50d572cf
Lauringschw <42889446+Lauringschw@users.noreply.github.com>
2020-05-17 11:56:44 +0200

    Update Welcome01.java

--- Welcome01.java
+++ Welcome01.java
@@ -1,12 +1,13 @@
-import core.data.*;
+import core.data.*; //import the data
 
 public class Welcome01 {
+	// main tester
 	public static void main(String[] args) {
 		String id = "KSEA";
 		DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/" + id + ".xml");
 		ds.setCacheTimeout(15 * 60);
 		ds.load();
-		// ds.printUsageString();
+		ds.printUsageString();
 		float temp = ds.fetchFloat("temp_f");
 		String loc = ds.fetchString("location");
 		System.out.println("The temperature at " + loc + " is " + temp + "F");
